2b4e00ffdaf5439fa8805f7e2eb3c2fea1eeee92
linhaiyang <>
2021-02-04 18:49:06 +0800

    0

---
+++ CJImageView-master/CJImageView/Main/UploaderController/View/YYTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  YYTableViewCell.h
+//  CJImageView
+//
+//  Created by shengkai li on 2021/2/3.
+//  Copyright © 2021 C.K.Lian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YYTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CJImageView-master/CJImageView/Main/UploaderController/View/YYTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  YYTableViewCell.m
+//  CJImageView
+//
+//  Created by shengkai li on 2021/2/3.
+//  Copyright © 2021 C.K.Lian. All rights reserved.
+//
+
+#import "YYTableViewCell.h"
+
+@implementation YYTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
